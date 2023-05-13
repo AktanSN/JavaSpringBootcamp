25d857950521c7e4dab83b4c57e520547846e0d4
Aktan Sanhal <aktansanhal@gmail.com>
2023-05-13 19:17:41 +0300

    homeworks added

---
+++ akbank-homework1/src/main/java/com/ibrahimaktansanhal/akbankhomework1/AkbankHomework1Application.java
@@ -0,0 +1,13 @@
+package com.ibrahimaktansanhal.akbankhomework1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkbankHomework1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkbankHomework1Application.class, args);
+	}
+
+}
